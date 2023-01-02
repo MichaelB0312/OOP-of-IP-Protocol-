4886ba2045290d46d53ee8ec5fd3ec58d0108252
michaelB0312 <mberko18@gmail.com>
2023-01-02 20:47:43 +0200

    last update of Michael

--- hw6/main_str.cpp
+++ hw6/main_str.cpp
@@ -4,7 +4,17 @@ using namespace std;
 #include <string.h>
 
 int main(){
+	
+	//int n;
+	//cin >> n;
+	//const char* c = new char[20];
+	//c = "  I Love Hapoel   ";
+	//String str(c);
+	//str = c;
+	String str5("   Yallla Hapoell!!  ");
+	String str6 = str5.trim();
+	int bool1 = str5.equals("   fuck maccabi");
+	cout << bool1;
 
-	String str;
 	return 0;
 }
