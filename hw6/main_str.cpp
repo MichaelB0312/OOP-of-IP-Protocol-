ab016c4054b28cdeefd1454dbcbc0daef2b146fa
michaelB0312 <mberko18@gmail.com>
2023-01-06 12:28:30 +0200

    Hopefully last version of String class

--- hw6/main_str.cpp
+++ hw6/main_str.cpp
@@ -11,14 +11,17 @@ int main(){
 	//c = "  I Love Hapoel   ";
 	//String str(c);
 	//str = c;
-	String str5("   Yallla Hapoell!!  ");
-	//const char* delimiters = "aHl";
+	String str5("245.197.45.445.78 ");
+	//const char* delimiters = ".";
 	String *output;
 	size_t size;
-	str5.split("aHl",&output,&size);
+	str5.split(".", &output,&size);
 	delete[] output;
 	//String str6 = str5.trim();
 	int bool1 = str5.equals("   fuck maccabi");
+
+	String str4("   Hapoel    ");
+	str4 =  str4.trim();
 	cout << bool1;
 
 	return 0;
