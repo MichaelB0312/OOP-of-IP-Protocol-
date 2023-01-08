eb4a3d7ef794c2e6c5e30f2b2fb6787e75f12f3c
michaelB0312 <mberko18@gmail.com>
2023-01-08 20:10:10 +0200

    minor change

--- hw6/ip.cpp
+++ hw6/ip.cpp
@@ -4,39 +4,13 @@ using namespace std;
 #include "ip.h"
 
 
-IP::IP(String packet, String law) {
+IP::IP(String law) {
 	
 	law = law.trim();
 	//find type of field from the law
 	field = find_field(law);
-	
-	/*extract the IP from the packet:*/
-	//1. divide to fields
-	String* out_fields;
-	size_t size_fields;
-	packet.split(",", &out_fields, &size_fields);
-
-	//2. divide each field according to '='
-	for (size_t i = 0; i < size_fields; i++) {
-
-		out_fields[i] = out_fields[i].trim();
-		String* tmp_out;
-		size_t tmp_size;
-		out_fields[i].split("=", &tmp_out, &tmp_size);
-		
-		//3.identify the corret ip according to field
-		if (tmp_out[0].trim().equals(field)) {
-			//4.then the second sub-field is the ip!
-			tmp_out[1] = tmp_out[1].trim();
-			ip = tmp_out[1];
-			delete[] tmp_out;
-			break;
-		}
-		delete[] tmp_out;
-	}
-	
-	delete[] out_fields;
-
+	//zero initiating. we'll find the accurate ip in set_value
+	ip_law = "0.0.0.0";
 
 }
 
@@ -56,45 +30,83 @@ String IP::find_field(String law) {
 	}
 }
 
+
+
 bool IP::set_value(String value) {
 
-	//find the required values using split with delimiters "/". use to_integer() for getting the value
-
-	//now we'll use get_mask from the sadna
-
-	//if(this->ip_to_int(char a,b,c,d)
-
-	unsigned int mask = get_mask(value);
 	
-	//now, we'll get the IP of the law (left to "/")
+	/*now, we'll get the IP of the law (left to "/")*/
 	String cpy_value = value;
 	String* equ_out;
 	size_t equ_size;
 
-	cpy.split("=",&equ_out,&equ)
+	cpy_value.split("=", &equ_out, &equ_size);
+	if (equ_out == NULL) {
+		return false;
+	}
+	String ip_and_range = equ_out[1].trim(); // our law is right to '='
+	delete[] equ_out;
 	
+	size_t slash_size;
+	String* slash_out;
+	ip_and_range.split("/", &slash_out, &slash_size);
+	if (slash_out == NUll) {
+		return false;
+	}
+	ip_law = slash_out[0]; //	IP is left to the '/'
+	delete[] slash_out;
 
 
-
+	return true;
 }
 
 
 bool IP::match(String packet) {
 
-	//get the law -> and split("/") in order to get the m (length of the mask)
+	if (set_value(law)) {
+		/*extract the IP from the packet according to field:*/
+		//1. divide to fields
+		String* out_fields;
+		size_t size_fields;
+		packet.split(",", &out_fields, &size_fields);
 
-	
-	if (this->set_value(law)) {
+		//2. divide each field according to '='
+		for (size_t i = 0; i < size_fields; i++) {
 
+			out_fields[i] = out_fields[i].trim();
+			String* tmp_out;
+			size_t tmp_size;
+			out_fields[i].split("=", &tmp_out, &tmp_size);
+
+			//3.identify the corret ip according to field
+			if (tmp_out[0].trim().equals(field)) {
+				//4.then the second sub-field is the ip!
+				tmp_out[1] = tmp_out[1].trim();
+				String ip_extern = tmp_out[1];
+				delete[] tmp_out;
+				break;
+			}
+			delete[] tmp_out;
+		}
+
+		delete[] out_fields;
 	}
+
+	/*now, we'll check matching between IP of law and IP of the outer packet*/
+	unsigned int mask = get_mask(law);
+	unsigned int ip_ext_int = ip_to_int(ip_extern);
+	unsigned int ip_law_int = ip_to_int(ip_law);
+
+	return (ip_ext_int & mask) == (ip_law_int & mask);
 }
 
 
-unsigned int IP::ip_to_int(String ip) {
+
+unsigned int IP::ip_to_int(String ext_ip) {
 
 	String* out;
 	size_t size;
-	ip.split(".", out, size);
+	ext_ip.split(".", out, size);
 	unsigned int arr[size];
 	for (size_t i = 0; i < size; i++) {
 		arr[i] = out[i].to_integer();
@@ -114,7 +126,7 @@ unsigned int IP::get_mask(String law) {
 	String cpy_law = law;
 	size_t size;
 	String* out_str;
-	cpy_law.split("/", &out_str, size);
+	cpy_law.split("/", &out_str, &size);
 	int m = out_str[1].to_integer();
 	delete[] out_str;
 
