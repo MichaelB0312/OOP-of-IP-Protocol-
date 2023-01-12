8b2a03418dcdf5e75d809988c73da74bacb47a40
michaelB0312 <mberko18@gmail.com>
2023-01-12 11:35:07 +0200

    finished BASH

---
+++ hw6/firewall.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+
+packets=$(cat -)
+
+#echo "$packets"
+non_hash=$(sed '/^[[:blank:]]*#/d;s/#.*//' "$1")
+
+non_space=$(echo "$non_hash" | sed '/^$/d')
+
+#sed '/^$/d' $non_hash > hashtag_clean.txt
+
+#echo "$non_space"
+
+
+for row in $non_space; do
+  law1=$(echo $row | awk -F, '{print $1}')
+  law2=$(echo $row | awk -F, '{print $2}')
+  law3=$(echo $row | awk -F, '{print $3}')
+  law4=$(echo $row | awk -F, '{print $4}')
+
+  #deliver packets to be filterd by firewall.exe
+  echo "$packets" | ./firewall.exe "$law1"\
+	  | ./firewall.exe "$law2"\
+	  | ./firewall.exe "$law3"\
+	  | ./firewall.exe "$law4"\
+	  | sort | cat  
+  #e02_filtered=$(echo "$packets" | ./firewall.exe "$law2")
+  #aw3_filtered=$(echo "$packets" | ./firewall.exe "$law3")
+  #aw4_filtered=$(echo "$packets" | ./firewall.exe "$law4")
+  #echo "$var1"
+  #echo "$var2"
+  #echo "$var3"
+  #echo "$var4"
+ 
+done     # > splitted_rules.txt
+
+
