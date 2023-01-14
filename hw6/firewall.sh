57e7fcc84b9777de4bf1ee079e624f86a92e09b4
michaelB0312 <mberko18@gmail.com>
2023-01-14 18:55:56 +0200

    finallll

--- hw6/firewall.sh
+++ hw6/firewall.sh
@@ -4,39 +4,21 @@
 
 packets=$(cat -)
 
-#echo "$packets"
-non_hash=$(sed '/^[[:blank:]]*#/d;s/#.*//' "$1")
+#removing hashtags and spaces
+non_hash_laws=$(sed '/^[[:blank:]]*#/d;s/#.*//' "$1")
 
-non_space=$(echo "$non_hash" | sed '/^$/d')
 
-#sed '/^$/d' $non_hash > hashtag_clean.txt
-
-#$echo "$non_space"
+non_space_laws=$(echo "$non_hash_laws" | sed '/^$/d')
 
 
 while read line; do
-  #echo "$line"
   law1=$(echo $line | awk -F, '{print $1}')
-  #echo "$law1" # > law1.txt
 
   law2=$(echo $line | awk -F, '{print $2}')
- # echo "$law2" # > law2.txt
 
   law3=$(echo $line | awk -F, '{print $3}')
-  #echo "$law1" # > law1.txt
 
   law4=$(echo $line | awk -F, '{print $4}')
-  #echo "$law1" # > law1.txt
-
-
-  #law2=$(echo $row | cut -d "," -f2)
-  #echo "$law2" > law2.txt
-
-  #law3=$(echo $row | cut -d "," -f3)
-  #echo "$law3" > law3.txt
-  #law4=$(echo $row | cut -d "," -f4)
-
-  #echo "$law4" > law4.txt
 
   #deliver packets to be filterd by firewall.exe
   filtered_pkts=$(echo "$packets" | ./firewall.exe "$law1" 2>/dev/null \
@@ -44,17 +26,9 @@ while read line; do
 	  | ./firewall.exe "$law3" 2>/dev/null \
 	  | ./firewall.exe "$law4" 2>/dev/null)
   
-  echo "$filtered_pkts"
-  valid_pkts+=$(echo $filtered_pkts)
-	 # | sort  
-  #e02_filtered=$(echo "$packets" | ./firewall.exe "$law2")
-  #aw3_filtered=$(echo "$packets" | ./firewall.exe "$law3")
-  #aw4_filtered=$(echo "$packets" | ./firewall.exe "$law4")
-  #echo "$var1"
-  #echo "$var2"
-  #echo "$var3"
-  #echo "$var4"
+  #echo "$filtered_pkts"
+  valid_pkts+=$(echo "\n$filtered_pkts")
  
-done <<< "$non_space"
+done <<< "$non_space_laws"
 
-echo "$valid_pkts"
+echo -e "$valid_pkts" | sort | sed '/^$/d' | sed 's/ //g'
