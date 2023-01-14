c2a3fe1f071b72abce0ca374910b48df3daadbfe
michaelB0312 <mberko18@gmail.com>
2023-01-14 12:00:25 +0200

    remove my checks

--- hw6/main.cpp
+++ hw6/main.cpp
@@ -43,10 +43,6 @@ int main(int argc, char** argv) {
 		return ERROR;
 	}
 
-	bool result = field->match("dst-ip=3.0.0.0");
-	cout << result;
-	//bool result = field->match("src-ip=1.1.1.1");
-	//cout << result;
 
 	parse_input(*field);
 	delete[] splitted_law;
